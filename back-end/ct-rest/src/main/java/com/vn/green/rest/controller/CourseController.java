b3cd97f496e32c7e4b02658a4dd00811b6328fd5
Vi Huynh <huynhtuongvi.cntt@gmail.com>
2021-05-29 18:29:24 +0700

    [ct_003] add no-auth for course controller

--- back-end/ct-rest/src/main/java/com/vn/green/rest/controller/CourseController.java
+++ back-end/ct-rest/src/main/java/com/vn/green/rest/controller/CourseController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/course")
+@RequestMapping(value = "/no-auth/course")
 public class CourseController {
 
     @Autowired
